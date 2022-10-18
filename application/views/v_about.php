73a21966c28f660f0f6ef86c87f26724a9cb2968
annisa67azizah <annisa67nurazizah@gmail.com>
2022-10-18 21:18:05 +0700

    project pertama

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>nama Depan : Annisa</li>
+        <li>Nama Belakang : Nur Azizah</li>
+    </ul>
+    <br>
+    <h4>Alamat></h4>
+    <ul type="none">
+        <li>Jl. Veteran III </li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Workout</li>
+    </ul>
+</section>
